31cb5fce568e43d0b4526bb0ea4e77422556d394
Andrew Beam <beam@npsvm2.med.harvard.edu>
2016-04-08 20:12:43 -0400

    basic w2v functionality added

--- src/patient2vec.py
+++ src/patient2vec.py
@@ -1,2 +1,14 @@
 # Main script #
-def load_data
+from gensim.models import Word2Vec
+import logging
+
+class line_iterator(object):
+    def __init__(self, fname):
+        self.fname = fname
+    def __iter__(self):
+        for line in open(self.fname):
+            yield line.split()
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+patients = line_iterator('../data/patient_walks.txt')
+model = Word2Vec(patients,size=10)
